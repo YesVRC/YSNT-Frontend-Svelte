f18d82a2d44c40e8c9dc9b33405f9ea10f87277a
YesVRC <altair9090@gmail.com>
2025-03-07 00:09:47 -0500

    meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,7 @@
 </script>
 
 <svelte:head>
-	<title>YSNT</title>
-	<meta charset="utf-8"/>
-	<meta property="og:title" content="Y S N T" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://ysnt.live" />
-	<meta property="og:image" content="https://ysnt.live/embed.png" />
-	<meta property="og:description" content="Yes's Website idk man" />
-	<meta name="theme-color" content="#000000">
 
-	<!-- Include this to make the og:image larger -->
-	<meta name="twitter:card" content="summary_large_image">
 </svelte:head>
 
 <NavBar/>
